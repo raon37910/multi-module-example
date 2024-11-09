d9054c539f78affd8ccc69675e085846d605e51d
김정민 <raon37910@gmail.com>
2024-11-10 08:57:52 +0900

    api 모듈 추가

---
+++ module-api/src/test/java/com/raon/moduleapi/ModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon.moduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
