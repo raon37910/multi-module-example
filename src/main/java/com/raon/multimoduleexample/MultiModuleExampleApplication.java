f5870e1e4e999cda1dfc3eef5cada7fbb1765674
김정민 <raon37910@gmail.com>
2024-11-10 08:52:55 +0900

    기본 프로젝트 세팅

---
+++ src/main/java/com/raon/multimoduleexample/MultiModuleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.raon.multimoduleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raon/multimoduleexample/MultiModuleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon.multimoduleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
